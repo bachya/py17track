328f4b85bfd8db8efeace7196ce0d948054d2389
Aaron Bach <bachya1208@gmail.com>
2018-12-05 22:01:21 -0700

    Add logging support

--- example.py
+++ example.py
@@ -1,30 +1,31 @@
 """Run an example script to quickly test a 17track.net account."""
 import asyncio
+import logging
 
 from aiohttp import ClientSession
 
 from py17track import Client
 from py17track.errors import SeventeenTrackError
 
+_LOGGER = logging.getLogger()
+
 
 async def main() -> None:
     """Create the aiohttp session and run the example."""
+    logging.basicConfig(level=logging.INFO)
+
     async with ClientSession() as websession:
         try:
             client = Client(websession)
+
             await client.profile.login('<EMAIL>', '<PASSWORD>')
+            _LOGGER.info('Account ID: %s', client.profile.account_id)
 
-            print('Account ID: {0}'.format(client.profile.account_id))
-
-            print()
-            print('Getting account summary...')
             summary = await client.profile.summary()
-            print(summary)
+            _LOGGER.info('Account Summary: %s', summary)
 
-            print()
-            print('Getting all account packages...')
             packages = await client.profile.packages()
-            print(packages)
+            _LOGGER.info('Package Summary: %s', packages)
         except SeventeenTrackError as err:
             print(err)
 
