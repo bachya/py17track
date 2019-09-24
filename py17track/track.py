df5be698fc5706f18243c374e72460c5f652f708
Aaron Bach <bachya1208@gmail.com>
2019-09-24 11:56:55 -0600

    Add Python 3.6-style variable type annotations (#11)

--- py17track/track.py
+++ py17track/track.py
@@ -1,10 +1,10 @@
 """Define interaction with an individual package."""
-from typing import Callable, Coroutine
+from typing import Callable, Coroutine, List
 
 from .errors import InvalidTrackingNumberError
 from .package import Package
 
-API_URL_TRACK = "https://t.17track.net/restapi/track"
+API_URL_TRACK: str = "https://t.17track.net/restapi/track"
 
 
 class Track:  # pylint: disable=too-few-public-methods
@@ -12,26 +12,24 @@ class Track:  # pylint: disable=too-few-public-methods
 
     def __init__(self, request: Callable[..., Coroutine]) -> None:
         """Initialize."""
-        self._request = request
+        self._request: Callable[..., Coroutine] = request
 
     async def find(self, *tracking_numbers: str) -> list:
         """Get tracking info for one or more tracking numbers."""
-        data = {"data": [{"num": num} for num in tracking_numbers]}
-        tracking_resp = await self._request("post", API_URL_TRACK, json=data)
-
-        print(tracking_resp)
+        data: dict = {"data": [{"num": num} for num in tracking_numbers]}
+        tracking_resp: dict = await self._request("post", API_URL_TRACK, json=data)
 
         if not tracking_resp.get("dat"):
             raise InvalidTrackingNumberError("Invalid data")
 
-        packages = []
+        packages: List[Package] = []
         for info in tracking_resp["dat"]:
-            package_info = info.get("track", {})
+            package_info: dict = info.get("track", {})
 
             if not package_info:
                 continue
 
-            kwargs = {
+            kwargs: dict = {
                 "destination_country": package_info.get("c"),
                 "info_text": package_info.get("z0", {}).get("z"),
                 "location": package_info.get("z0", {}).get("c"),
