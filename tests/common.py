ea61161be28fa2c0e7fe76355760d684f96db813
Aaron Bach <bachya1208@gmail.com>
2020-01-29 12:55:50 -0700

    Modernize test and fixture framework (#18)

---
+++ tests/common.py
@@ -0,0 +1,12 @@
+"""Define common test utilities."""
+import os
+
+TEST_EMAIL = "user@email.com"
+TEST_PASSWORD = "password"
+
+
+def load_fixture(filename):
+    """Load a fixture."""
+    path = os.path.join(os.path.dirname(__file__), "fixtures", filename)
+    with open(path, encoding="utf-8") as fptr:
+        return fptr.read()
--- tests/const.py
+++
@@ -1,3 +0,0 @@
-"""Define constants for use in tests."""
-TEST_EMAIL = "user@email.com"
-TEST_PASSWORD = "password"
